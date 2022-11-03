dc083939636b68337a2dda1b6680539303760f30
Omer Menekse <omermenekse@gmail.com>
2022-11-03 05:35:55 +0300

    new

---
+++ src/ders1/Ders2.java
@@ -0,0 +1,7 @@
+package ders1;
+
+public class Ders2 {
+    public static void main(String[] args) {
+        System.out.println("bakalım oldumu");
+    }
+}
--- src/ders1/ttest.java
+++ src/ders1/ttest.java
@@ -3,5 +3,6 @@ package ders1;
 public class ttest {
     public static void main(String[] args) {
         System.out.println(11+1);
+        System.out.println("naber");
     }
 }
